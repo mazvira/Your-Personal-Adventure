716c8d7160a88dc440f193f5e1e58aadc007c011
vikuuushnir <50868925+vikuuushnir@users.noreply.github.com>
2019-06-10 15:05:13 +0300

    Add files via upload

---
+++ java/com/esri/android/nearbyplaces/ApplicationTest.java
@@ -0,0 +1,8 @@
+package com.esri.android.nearbyplaces;
+import android.app.Application;
+import android.test.ApplicationTestCase;
+public class ApplicationTest extends ApplicationTestCase<Application> {
+  public ApplicationTest() {
+    super(Application.class);
+  }
+}
